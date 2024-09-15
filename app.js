a4759a93f5de79b30afd7e89ca679daa55db8ce7
amirha <hasanabadi.am1380@gmail.com>
2024-09-15 16:33:27 +0330

    server created & connected to database

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 require("dotenv").config();
 const logger = require("pino")();
+const express = require("express");
+const mongoose = require("mongoose");
 
-logger.info(process.env.Port)
+// .env variables
+const Port = process.env.PORT || 4000;
+const Database_url = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/reminder";
+
+// Creating App
+const app = express();
+
+// Database Connection - Creating server
+mongoose.connect(Database_url).then(() => {
+  app.listen(Port, () => {
+    logger.info(`Server is running on port ${Port}`);
+  });
+});
